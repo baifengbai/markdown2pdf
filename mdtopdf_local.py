3108b66898589639afe5f4bdc1c164dc269b6395
baifeng <342741263@qq.com>
2019-07-31 19:04:21 +0800

    add main()

--- mdtopdf_local.py
+++ mdtopdf_local.py
@@ -1,10 +1,12 @@
 import glob
 import os
-
-files = glob.glob('./*.md')
-with open('tmp.md', 'w+') as f:
-    for i in files:
-        f.write(open(i).read())
-os.system('md2pdf tmp.md test.pdf')  # 需要安装md2pdf
-os.remove('tmp.md')
-print('finished!')
+def main():
+    files = glob.glob('./*.md')
+    with open('tmp.md', 'w+') as f:
+        for i in files:
+            f.write(open(i).read())
+    os.system('md2pdf tmp.md test.pdf')  # 需要安装md2pdf
+    os.remove('tmp.md')
+    print('finished!')
+if __name__ == '__main__':
+    main()
